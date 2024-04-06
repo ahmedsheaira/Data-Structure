df3fbe21b47aaf60cec27a6d9e7841b634fa018e
ahmedsheaira <sheaira.ahmed@gmail.com>
2024-04-06 19:16:41 +0200

    Test Single Linked List using C++

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "Array/include/array.hpp"
+#include "LinkedList/include/linked_list.hpp"
 
 int main() {
     cout << "This is Array ADT demo!!" << endl;
@@ -29,6 +30,32 @@ int main() {
     cout << "The size of Array -> " << myarray.get_size_arr() << endl;
     cout << "The length of Array -> " << myarray.get_length_arr() << endl;
     myarray.display_arr();
+
+    cout << "This is Single Linked List demo!!" << endl;
+    SingleLinkedList sll;
+    sll.append_sll(4);
+    sll.display_sll();
+    cout << "The Length of the Single Linkde List: " << sll.get_length_sll() << endl;
+    sll.push_sll(10);
+    sll.push_sll(20);
+    sll.display_sll();
+    cout << "The Length of the Single Linkde List: " << sll.get_length_sll() << endl;
+    sll.append_sll(43);
+    sll.append_sll(28);
+    sll.display_sll();
+    cout << "The Length of the Single Linkde List: " << sll.get_length_sll() << endl;
+    sll.insert_sll(6, 3);
+    sll.display_sll();
+    cout << "The Length of the Single Linkde List: " << sll.get_length_sll() << endl;
+    sll.remove_sll(3);
+    sll.display_sll();
+    cout << "The Length of the Single Linkde List: " << sll.get_length_sll() << endl;
+    sll.insert_sll(16, 5);
+    sll.display_sll();
+    cout << "The Length of the Single Linkde List: " << sll.get_length_sll() << endl;
+    sll.remove_sll(4);
+    sll.display_sll();
+    cout << "The Length of the Single Linkde List: " << sll.get_length_sll() << endl;
 
     return 0;
 }
