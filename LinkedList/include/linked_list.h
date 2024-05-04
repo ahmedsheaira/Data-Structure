c07a69c345e17f8885036e1b4f41c2be6f0524de
ahmedsheaira <sheaira.ahmed@gmail.com>
2024-05-05 02:14:04 +0300

    Add Doubly Linked List Using C

--- LinkedList/include/linked_list.h
+++ LinkedList/include/linked_list.h
@@ -24,12 +24,18 @@ void remove_sll(struct NodeSLL** head, int index);
 int get_length_sll(struct NodeSLL* head);
 void display_sll(struct NodeSLL* head);
 
-// struct NodeDLL {
-//     int data;
-//     struct NodeDLL* next;
-//     struct NodeDLL* prev;
-// };
+struct NodeDLL {
+    int data;
+    struct NodeDLL* next;
+    struct NodeDLL* prev;
+};
 
-
+void push_dll(struct NodeDLL** head, int data);
+void append_dll(struct NodeDLL** head, int data);
+void insert_dll(struct NodeDLL** head, int data, int index);
+void remove_dll(struct NodeDLL** head, int index);
+int get_length_dll(struct NodeDLL* head);
+void display_dll(struct NodeDLL* head);
+void display_rev_dll(struct NodeDLL* head);
 
 #endif
--- LinkedList/linked_list.c
+++ LinkedList/linked_list.c
@@ -103,3 +103,120 @@ void display_sll(struct NodeSLL* head) {
     }
     printf("\n");
 }
+
+void push_dll(struct NodeDLL** head, int data) {
+    struct NodeDLL* node = (struct NodeDLL*)malloc(sizeof(struct NodeDLL));
+    node->data = data;
+    node->next = (*head);
+    node->prev = NULL;
+    if ((*head) != NULL)
+        (*head)->prev = node;
+    (*head) = node;
+}
+
+void append_dll(struct NodeDLL** head, int data){
+    if ((*head) == NULL) {
+        push_dll(head, data);
+        return;
+    }
+    struct NodeDLL* node = (struct NodeDLL*)malloc(sizeof(struct NodeDLL));
+    node->data = data;
+    node->next = NULL;
+    struct NodeDLL* itr = (*head);
+    while (itr->next) {
+        itr = itr->next;
+    }
+    itr->next = node;
+    node->prev = itr;
+}
+
+void insert_dll(struct NodeDLL** head, int data, int index) {
+    if ((index < 0) || (index > get_length_dll(*head))) {
+        printf("Invalid Index!!\n");
+        return;
+    }
+    if (index == 0) {
+        push_dll(head, data);
+        return;
+    }
+    struct NodeDLL* node = (struct NodeDLL*)malloc(sizeof(struct NodeDLL));
+    node->data = data;
+    int count = 0;
+    struct NodeDLL* itr = (*head);
+    while (itr) {
+        if (count == index - 1) {
+            node->next = itr->next;
+            node->prev = itr;
+            itr->next = node;
+            itr->next->next->prev = node;
+            break;
+        }
+        itr = itr->next;
+        count++;
+    }
+}
+
+void remove_dll(struct NodeDLL** head, int index) {
+    if ((index < 0) || (index > get_length_dll(*head))) {
+        printf("Invalid Index!!\n");
+        return;
+    }
+    struct NodeDLL* itr = (*head);
+    if (index == 0) {
+        (*head) = (*head)->next;
+        (*head)->prev = NULL;
+        free(itr);
+        return;
+    }
+    int count = 0;
+    while (itr) {
+        if (count == index - 1) {
+            struct NodeDLL* node = itr->next;
+            itr->next = itr->next->next;
+            itr->next->prev = itr;
+            free(node);
+            break;
+        }
+        itr = itr->next;
+        count++;
+    }
+}
+
+int get_length_dll(struct NodeDLL* head) {
+    int count = 0;
+    struct NodeDLL* itr = head;
+    while (itr) {
+        count++;
+        itr = itr->next;
+    }
+    return count;
+}
+
+void display_dll(struct NodeDLL* head) {
+    if (head == NULL) {
+        printf("Doubly Linked List is empty!!\n");
+        return;
+    }
+    struct NodeDLL* itr = head;
+    while (itr) {
+        printf("%d", itr->data);
+        if (itr->next)
+            printf("-->");
+        itr = itr->next;
+    }
+    printf("\n");
+}
+
+void display_rev_dll(struct NodeDLL* head) {
+    struct NodeDLL* itr = head;
+    while (itr->next) {
+        itr = itr->next;
+    }
+    while (itr) {
+        printf("%d", itr->data);
+        if (itr->prev)
+            printf("<--");
+        itr = itr->prev;
+    }
+    printf("\n");
+}
